66850e311111af6092f803f8220ddf690d61461b
Matt Laptop <matthewisawzome@gmail.com>
2024-05-04 14:21:46 +1000

    added some fun stuff

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+// OBJECTS
+
 // RENDERER
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight);
@@ -27,40 +29,36 @@ const material = new THREE.MeshBasicMaterial( { color:0xFFFFFF } );
 const sphere = new THREE.Mesh( geometry, material );
 scene.add( sphere );
 
-// AXES HELPER
-const axes = new THREE.AxesHelper( 5 );
-scene.add( axes );
-
 // RAYCASTER
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 
 function animate() {
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
     renderer.render( scene, camera );
-    requestAnimationFrame( animate );
-} animate();
+} 
+renderer.setAnimationLoop(animate);
 
-function resize() {
+window.addEventListener( 'resize', function() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
-}
-window.addEventListener( 'resize', resize() );
+} );
 
-function onClick ( event ) {
-    console.log("Mouse CLICKED");
+function onMouseDown ( event ) {
+    console.log("Mouse DOWN");
     pointer.set(
         (event.clientX / window.innerWidth) * 2 - 1,
         -(event.clientY / window.innerHeight) * 2 + 1
     );
 
     raycaster.setFromCamera( pointer, camera );
-    const intersects = raycaster.intersectObjects( scene.children, false);
+    const intersects = raycaster.intersectObjects( scene.children );
     if( intersects.length > 0 ) {
         const intersect = intersects[0];
         camera.position.copy(intersect.point);
-        intersect.color(0x00FF22);
+        intersect.object.material.color = new THREE.Color(0xaa6633);
     }
 }
-document.addEventListener( 'onClick', onClick );
-
+document.addEventListener( 'mousedown', onMouseDown );
