24d90f1c41df56c7ab94908f8a6404bff0e071bf
joooood <83745317+joooood@users.noreply.github.com>
2024-09-09 19:57:02 +1000

    changed how rotation works, added textures

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-// OBJECTS
+let targetRotationX = 0.005;
+let targetRotationY = 0.02;
+let mouseX = 0, mouseXOnMouseDown = 0, mouseY = 0, mouseYOnMouseDown = 0;
+const windowHalfX = window.innerWidth / 2;
+const windowHalfY = window.innerHeight / 2;
+const dragFactor = 0.0002;
+const slowingFactor = 0.98;
 
+function onDocumentMouseDown (event) {
+    event.preventDefault();
+    document.addEventListener('mousemove', onDocumentMouseMove, false );
+    document.addEventListener('mouseup', onDocumentMouseUp, false );
+    mouseXOnMouseDown = event.clientX - windowHalfX;
+    mouseYOnMouseDown = event.clientY - windowHalfY;
+}
+
+function onDocumentMouseMove( event ) {
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+    targetRotationX = ( mouseX - mouseXOnMouseDown ) * dragFactor;
+    targetRotationY = ( mouseY - mouseYOnMouseDown ) * dragFactor;
+}
+
+function onDocumentMouseUp( event ) {
+    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+}
 
 // RENDERER
 const renderer = new THREE.WebGLRenderer();
@@ -11,13 +36,13 @@ document.body.appendChild( renderer.domElement );
 
 // SCENE
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x004455);
+scene.background = new THREE.Color(0xFFFFFF);
 
 // CAMERA
 const camera = new THREE.PerspectiveCamera(
     75, window.innerWidth / window.innerHeight, 0.1, 1000
 );
-camera.position.z = 5;
+camera.position.z = 1.0;
 
 // LIGHT
 const ambientLight = new THREE.AmbientLight(0xFFFFFF);
@@ -28,19 +53,28 @@ const controls = new OrbitControls(
     camera, renderer.domElement 
 );
 
-// SPHERE
-const geometry = new THREE.SphereGeometry( 2.5 );
-const material = new THREE.MeshBasicMaterial( { color:0x554455 } );
-const sphere = new THREE.Mesh( geometry, material );
-scene.add( sphere );
-
 // RAYCASTER
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 
+// OBJECTS
+// EARTH
+const earthGeometry = new THREE.SphereGeometry( 0.5, 32, 32 );
+const earthMaterial = new THREE.MeshPhongMaterial(
+     {  
+        wireframe: false,
+        map: new THREE.TextureLoader().load('texture/EarthMap.jpg'),
+        bumpMap: new THREE.TextureLoader().load('texture/EarthBump.jpg'),
+        bumpScale: 0.01
+     } );
+const earthMesh = new THREE.Mesh( earthGeometry, earthMaterial );
+scene.add( earthMesh );
+
 function animate() {
-    sphere.rotation.x += 0.01;
-    sphere.rotation.y += 0.01;
+    earthMesh.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), targetRotationX);
+    earthMesh.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), targetRotationY);
+    targetRotationX *= slowingFactor;
+    targetRotationY *= slowingFactor;
     renderer.render( scene, camera );
 }
 renderer.setAnimationLoop(animate);
@@ -51,6 +85,7 @@ window.addEventListener( 'resize', function() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 } );
 
+/*
 function onMouseDown ( event ) {
     console.log("Mouse DOWN");
     pointer.set(
@@ -62,7 +97,8 @@ function onMouseDown ( event ) {
     const intersects = raycaster.intersectObjects( scene.children, false );
     if( intersects.length > 0 ) {
         const intersect = intersects[0];
-        intersect.object.material.color = new THREE.Color( 0xaa6633 );
     }
 }
-document.addEventListener( 'mousedown', onMouseDown );
+    */
+document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+window.onload = main;
